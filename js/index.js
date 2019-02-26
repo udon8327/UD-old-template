f096d77c5ff71fbbdfc41538d4f2a5f0b997c2af
Bohan <bohan.huang@coolbe.com.tw>
2019-02-26 09:41:21 +0800

    v1.0.0

---
+++ js/index.js
@@ -0,0 +1,14 @@
+//gotop
+$(function(){
+  $('#gotop').click(function(){
+    $('html,body').animate({scrollTop: 0}, 500);
+  });
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 300){
+      $('#gotop').fadeIn('fast');
+    }
+    else{
+      $('#gotop').stop().fadeOut('fast');
+    }
+  });
+})
